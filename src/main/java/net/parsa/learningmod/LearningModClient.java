fa2c14bc439d4058b981cec5a6abb34945816b6d
r4ype <raypemaster@gmail.com>
2024-05-28 13:29:50 +0330

    Initial commit

---
+++ src/main/java/net/parsa/learningmod/LearningModClient.java
@@ -0,0 +1,10 @@
+package net.parsa.learningmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
